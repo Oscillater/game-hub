50138e2ed15d3218423f23d807d512da19bff241
Oscillater <chetianwen@163.com>
2024-08-13 19:15:00 +0800

    filter platform

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,15 @@
 import { Center, SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
-interface Props{
-  selectedGenre:Genre |null;
+interface Props {
+  selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
 }
-const GameGrid = ({selectedGenre}:Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -16,7 +17,7 @@ const GameGrid = ({selectedGenre}:Props) => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={3}   
+        spacing={3}
         padding="10px"
       >
         {isLoading &&
