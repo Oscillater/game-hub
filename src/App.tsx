1142ec926e87dd117edee5180437f504bdfa9215
Oscillater <chetianwen@163.com>
2024-08-15 16:25:47 +0800

    page heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Game, Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -39,7 +40,9 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
+        <Box paddingLeft={2}>
+        <GameHeading gameQuery={GameQuery}/>
+        <HStack spacing={5}  marginBottom={5}>
         <PlatformSelector
           selectedPlatform={GameQuery.platform}
           onSelectPlatform={(platform) =>
@@ -48,6 +51,7 @@ function App() {
         />
         <SortSelector sortSelected={GameQuery.sortOrder} onSelectSortOrder={(order)=>setGameQuery({...GameQuery,sortOrder:order})}/>
         </HStack>
+        </Box>
         <GameGrid
           gameQuery={GameQuery}
         />
