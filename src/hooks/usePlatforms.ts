5ed4301a8d9f018b75db8e002c483a2a9296689b
Oscillater <chetianwen@163.com>
2024-08-15 18:01:04 +0800

    process data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,10 @@
-import useData from "./useData";
+import platform from "../data/platform";
 
 interface Platform{
     id:number;
     name:string;
     slug:string;
 }
-const usePlatforms=()=>useData<Platform>('/platforms/lists/parents');
+const usePlatforms=()=>({data:platform,Loading:false,error:null})
 
 export default usePlatforms;
